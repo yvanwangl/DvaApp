b199450cff3253d5129966bc7eda89789fa65b28
wangyafei <wangyafei@startimes.com.cn>
2016-12-25 21:28:11 +0800

    modify styles

--- src/models/users.js
+++ src/models/users.js
@@ -1,6 +1,6 @@
-import { hashHistory } from 'dva/router';
-import { query, create, modify, del } from '../services/users';
-import { parse } from 'qs';
+import {hashHistory} from 'dva/router';
+import {query, create, modify, del} from '../services/users';
+import {parse} from 'qs';
 
 export default {
 
@@ -9,8 +9,8 @@ export default {
     state: {
         list: [],
         total: null,
-        field:'',
-        keyword:'',
+        field: '',
+        keyword: '',
         loading: false,
         current: null,
         currentItem: {},
@@ -20,11 +20,11 @@ export default {
 
     subscriptions: {
         setup({dispatch, history}) {
-            history.listen(location=>{
-                if(location.pathname == '/users'){
+            history.listen(location=> {
+                if (location.pathname == '/users') {
                     dispatch({
-                        type:'query',
-                        payload:location.query,
+                        type: 'query',
+                        payload: location.query,
                     });
                 }
             });
@@ -33,16 +33,16 @@ export default {
 
     effects: {
         *query({payload}, {call, put}){
-            yield put({ type: 'showLoading' });
+            yield put({type: 'showLoading'});
             yield put({
-                type:'updateQueryKey',
-                payload:{page:1, field:'', keyword:'', ...payload}
+                type: 'updateQueryKey',
+                payload: {page: 1, field: '', keyword: '', ...payload}
             });
-            const { data } = yield call(query, parse(payload));
-            if(data){
+            const {data} = yield call(query, parse(payload));
+            if (data) {
                 yield put({
-                    type:'querySuccess',
-                    payload:{
+                    type: 'querySuccess',
+                    payload: {
                         list: data.data,
                         total: data.page.total,
                         current: data.page.current
@@ -51,12 +51,12 @@ export default {
             }
         },
         *create({payload}, {call, put}){
-            yield put({ type:'hideMask' });
-            yield put({ type:'showLoading' });
-            const { data } = yield call(create, payload);
-            if(data&& data.success){
+            yield put({type: 'hideMask'});
+            yield put({type: 'showLoading'});
+            const {data} = yield call(create, payload);
+            if (data && data.success) {
                 yield put({
-                    type:'createSuccess',
+                    type: 'createSuccess',
                     payload: {
                         list: data.data,
                         total: data.page.total,
@@ -67,25 +67,25 @@ export default {
                 });
             }
         },
-        *modify({payload}, { select, call, put }){
-            yield put({type:'hideMask'});
-            yield put({type:'showLoading'});
-            const id = yield select(({ users }) => users.currentItem.id);
+        *modify({payload}, {select, call, put}){
+            yield put({type: 'hideMask'});
+            yield put({type: 'showLoading'});
+            const id = yield select(({users}) => users.currentItem.id);
             const newUser = {...payload, id};
-            const { data } = yield call(modify, newUser);
-            if(data && data.success){
+            const {data} = yield call(modify, newUser);
+            if (data && data.success) {
                 yield put({
-                    type:'modifySuccess',
-                    payload:newUser
+                    type: 'modifySuccess',
+                    payload: newUser
                 });
             }
         },
         *del({payload}, {call, put}){
-            yield put({ type:'showLoading' });
-            const { data } = yield call(del, {id:payload});
-            if(data&& data.success){
+            yield put({type: 'showLoading'});
+            const {data} = yield call(del, {id: payload});
+            if (data && data.success) {
                 yield put({
-                    type:'delSuccess',
+                    type: 'delSuccess',
                     payload,
                 });
             }
@@ -97,31 +97,31 @@ export default {
             return {...state, ...action.payload};
         },
         showLoading(state){
-            return {...state, loading:true}
+            return {...state, loading: true}
         },
         /*hideLoading(state){
-            return {...state, loading:false};
-        },*/
+         return {...state, loading:false};
+         },*/
         showMask(state, action){
-            return {...state, ...action.payload, maskVisible:true};
+            return {...state, ...action.payload, maskVisible: true};
         },
         hideMask(state){
-            return {...state, maskVisible:false};
+            return {...state, maskVisible: false};
         },
         querySuccess(state, action){
-            return {...state, ...action.payload, loading:false};
+            return {...state, ...action.payload, loading: false};
         },
         createSuccess(state, action){
-            return {...state, ...action.payload, loading:false};
+            return {...state, ...action.payload, loading: false};
         },
         modifySuccess(state, action){
             const updateUser = action.payload;
-            const newList = state.list.map(user=>user.id===updateUser.id?{...user,...updateUser}:user);
-            return {...state, list:newList, loading:false};
+            const newList = state.list.map(user=>user.id === updateUser.id ? {...user, ...updateUser} : user);
+            return {...state, list: newList, loading: false};
         },
         delSuccess(state, action){
-            const newList = state.list.filter(user=>user.id!==action.payload);
-            return {...state, list: newList, loading:false};
+            const newList = state.list.filter(user=>user.id !== action.payload);
+            return {...state, list: newList, loading: false};
         },
         updateQueryKey(state, action){
             return {...state, ...action.payload};
--- src/services/users.js
+++ src/services/users.js
@@ -8,23 +8,23 @@ export async function query(params) {
     return request(`/api/users?${qs.stringify(params)}`);
 }
 
-export async function create(params){
+export async function create(params) {
     return request('/api/users', {
-        method:'post',
+        method: 'post',
         body: qs.stringify(params)
     });
 }
 
 export async function modify(params) {
     return request('/api/users', {
-        method:'put',
+        method: 'put',
         body: qs.stringify(params)
     });
 }
 
 export async function del(params) {
     return request('/api/users', {
-        method:'delete',
+        method: 'delete',
         body: qs.stringify(params)
     });
 }
